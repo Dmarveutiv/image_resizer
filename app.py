49d05303479fdaf2a7379e510b16084c92ef0df6
Kilanko Daniel <danielkilanko14@gmail.com>
2026-07-30 12:54:06 +0100

    www

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, send_file
 from PIL import Image, ImageOps
 import io
+import os
 
 app = Flask(__name__)
 
@@ -87,4 +88,5 @@ def resize():
                       download_name=filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
